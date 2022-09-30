c07803da37b3744734605364194a75c4f108e364
mattrichor <matt.a.geyer@gmail.com>
2022-09-30 12:31:57 -0400

    edited about me section

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -7,22 +7,21 @@ const AboutMe = () => {
       </div>
       <div className="about-paragraph">
         <h4 className="thanks">Thank you for visiting my page!</h4>I'm a
-        Software dev who's recently graduated from General Assembly's Software
-        Engineering Immersive bootcamp with a passion to merge creativity with
-        the art of problem-solving.{' '}
+        Software dev, graduate from General Assembly's Software Engineering
+        Immersive bootcamp, and lifelong learner with a passion to merge
+        creativity with the art of problem-solving.
         <div className="blurb">
-          Although not the route I originally embarked down with primary school
-          & college, the world of tech has always piqued my interest, as I grew
-          up with a software engineer father (who always likes to tell me about
-          how much harder it is to work in Perl / cold fusion than
-          JavaScript...)
+          While my initial career trajectory saw me flying jets for an Airline,
+          The Tech world has always piqued my interest. I grew up with a
+          software engineer father (who likes to tell me about how much harder
+          it is to work in Perl / cold fusion than JavaScript...) & spent
+          countless hours as a teenager modding video games!
         </div>
         My background as an airline pilot and flight instructor fostered a
         rigorous appetite for all sorts of things that move and the ways in
-        which they work. I am currently applying the skills I've gained in my
-        journey to build challenging & fulfilling projects.
+        which they work.
         <br></br>In my spare time you’ll find me ripping the guitar in a live
-        setting, riding in a bike lane, or competitively racing on the weekends.
+        setting, riding in a bike lane, or competitively racing on the weekends!
         <br></br>Let's build something together :)
       </div>
     </div>
